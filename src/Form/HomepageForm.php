ba046e00d866d4b63a9509f1c44a7ce77d01ffd5
pierre27.ambroise <pierre27.ambroise@gmail.com>
2023-04-03 09:23:37 +0200

    update homepage

---
+++ src/Form/HomepageForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CSE;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class HomepageForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('presentationHome', CKEditorType::class, [
+                'label' => "Message sur l'accueil :",
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => ['class' => "offer-button"],
+                'label' => "Valider"
+            ]);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CSE::class,
+        ]);
+    }
+}
