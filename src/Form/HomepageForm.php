922847764c66d77fede2bacc9613eabffc9955f5
pierre27.ambroise <pierre27.ambroise@gmail.com>
2023-04-05 10:06:01 +0200

    fix style

--- src/Form/HomepageForm.php
+++ src/Form/HomepageForm.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class HomepageForm extends AbstractType
@@ -16,6 +17,9 @@ final class HomepageForm extends AbstractType
         $builder
             ->add('presentationHome', CKEditorType::class, [
                 'label' => "Message sur l'accueil :",
+                'constraints' => new Assert\NotBlank([
+                    'message' => 'Entrez un message',
+                ]),
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => "offer-button"],
