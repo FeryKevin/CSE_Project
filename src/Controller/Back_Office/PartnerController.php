7e7a92c929796cb8bd4ad7d2b80960cd57a4e7d8
FeryKevin <kevin.fery@outlook.com>
2023-04-14 14:43:15 +0200

    update v1

--- src/Controller/Back_Office/PartnerController.php
+++ src/Controller/Back_Office/PartnerController.php
@@ -24,6 +24,33 @@ class PartnerController extends AbstractController
         ]);
     }
 
+    #[Route(path: '/partners/delete', name: 'partner_delete', methods: ['POST'])]
+    public function delete(PartnerRepository $partnerRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        $post = json_decode($request->getContent(), true);
+
+        $em->remove($partnerRepository->find($post['id']));
+        $em->flush();
+
+        return new Response('Partenaire has been deleted');
+    }
+
+    #[Route(path: '/partners/update', name: 'partner_update', methods: ['POST'])]
+    public function updatePartner(PartnerRepository $partnerRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        $post = json_decode($request->getContent(), true);
+
+        $partner = $partnerRepository->find($post['id']);
+        $partner->setName($post['name'] !== '' ? $post['name'] : $partner->getName())
+            ->setDescription($post['description'] !== '' ? $post['description'] : $partner->getDescription())
+            ->setWebsiteLink($post['websiteLink'] !== '' ? $post['websiteLink'] : $partner->getWebsiteLink());
+
+        $em->persist($partner);
+        $em->flush();
+
+        return new Response('Partner has been udpate');
+    }
+
     #[Route(path: '/partners/add', name: 'partner_create')]
     public function create(Request $request, EntityManagerInterface $em): Response
     {
@@ -47,14 +74,20 @@ class PartnerController extends AbstractController
         return $this->render('back_office/partner/create.html.twig', ['form' => $form]);
     }
 
-    #[Route(path: '/partners/delete', name: 'partner_delete', methods: ['POST'])]
-    public function delete(PartnerRepository $partnerRepository, Request $request, EntityManagerInterface $em): Response
+    #[Route(path: '/partners/img/{id}', name: 'change_partner_image', methods: ['POST'])]
+    public function changePartner(PartnerRepository $partnerRepository, Request $request, EntityManagerInterface $em, Partner $partner): Response
     {
         $post = json_decode($request->getContent(), true);
 
-        $em->remove($partnerRepository->find($post['id']));
+        $partner->getImage()->setFile($request->files->get('image'));
+        $partner->getImage()->handlePartner();
+
+        $path = $partner->getImage()->getFile()->getRealPath();
+        move_uploaded_file($path, '.' . $partner->getImage()->getPath());
+
+        $em->persist($partner);
         $em->flush();
 
-        return new Response('Partenaire has been deleted');
+        return new Response('Image has been udpate');
     }
 }
